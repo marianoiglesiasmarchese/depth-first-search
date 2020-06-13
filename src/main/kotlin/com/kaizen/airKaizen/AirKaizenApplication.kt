d3dcc942ce28dee80f8336e7b3740ee66ba865f1
mariano iglesias marchese <marianoiglesiasmarchese@gmail.com>
2020-06-13 13:35:01 -0300

    basic project configuration

---
+++ src/main/kotlin/com/kaizen/airKaizen/AirKaizenApplication.kt
@@ -0,0 +1,13 @@
+package com.kaizen.airKaizen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirKaizenApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirKaizenApplication>(*args)
+}
+
+
---
+++ src/test/kotlin/com/kaizen/airKaizen/AirKaizenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaizen.airKaizen
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirKaizenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
