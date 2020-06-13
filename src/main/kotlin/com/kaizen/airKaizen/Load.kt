b6f303a8918f321f5fbe3cf61dd0f777ca1cdd07
mariano iglesias marchese <marianoiglesiasmarchese@gmail.com>
2020-06-13 16:22:20 -0300

    load improvement

--- src/main/kotlin/com/kaizen/airKaizen/Load.kt
+++ src/main/kotlin/com/kaizen/airKaizen/Load.kt
@@ -3,37 +3,66 @@ package com.kaizen.airKaizen
 import com.kaizen.airKaizen.model.Node
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.*
 
 
 @Configuration
-class Load(){
+class Load() {
+
+    companion object {
+        const val COMMA_DELIMITER = ","
+    }
+
+    val graph = arrayListOf<Node>()
 
     /**
      * Constraint
-     *
-     * //All flights are bi-directional; AirKaizen serves to and from all cities
-    //● AirKaizen uses the same identifier for a flight between two cities. So Chicago → Grand
-    //Rapids and Grand Rapids → Chicago both share flight id KZ01.
+     * - All flights are bi-directional; AirKaizen serves to and from all cities
+     * - AirKaizen uses the same identifier for a flight between two cities. So Chicago → Grand Rapids and Grand Rapids → Chicago both share flight id KZ01.
      */
     @Bean(name = ["graph"])
     fun loadData(): List<Node> {
 
-        // build node
-        var node1 = Node(name = "1")
-        var node2 = Node(name = "2")
-        var node3 = Node(name = "3")
-        var node4 = Node(name = "4")
-        var node5 = Node(name = "5")
+        val records: MutableList<List<String>> = ArrayList()
+        try {
+            val file = FileReader(
+                javaClass.classLoader.getResource("flights.csv").file
+            )
+            BufferedReader(file).use { br ->
+                var line: String
+                while (br.readLine().also { line = it } != null) {
+                    val values = line.split(COMMA_DELIMITER).toTypedArray()
+                    records.add(values.toList())
+                    println(records)
+                    process(values[0], values[1])
+                }
+            }
+        } catch (e: Exception){
+            println(e.stackTrace)
+        }
 
-        // load edges
-        node1.edges.addAll(listOf(node2, node3, node4, node5))
-        node2.edges.addAll(listOf(node1, node3, node4, node5))
-        node3.edges.addAll(listOf(node2, node1, node4, node5))
-        node4.edges.addAll(listOf(node2, node3, node1, node5))
-        node5.edges.addAll(listOf(node2, node3, node4, node1))
+        return graph
+    }
 
-        // build vertices
-        return listOf(node1, node2, node3, node4, node5)
+    private fun process(origin: String, destination: String) {
+        var originNode : Node = graph.find { it.name == origin } ?: Node(name = origin)
+        var destinationNode : Node = graph.find { it.name == destination } ?: Node(name = destination)
+        // check if one of them is new and create it
+        if (graph.none { it.name == origin }){
+            graph.add(originNode)
+        }
+        if (graph.none { it.name == destination }){
+            graph.add(destinationNode)
+        }
+        // add the relationship
+        if (originNode.edges.none { it.name == destinationNode.name }){
+            originNode.edges.add(destinationNode)
+        }
+        if (destinationNode.edges.none { it.name == originNode.name }){
+            destinationNode.edges.add(originNode)
+        }
     }
 
 }
