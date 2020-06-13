f4067d271b5f187f5c441ed05ebaec97bba29bb2
mariano iglesias marchese <marianoiglesiasmarchese@gmail.com>
2020-06-13 17:34:38 -0300

    refactor airkaizenservice to routeservice, code improvement

--- src/main/kotlin/com/kaizen/airKaizen/service/RouteService.kt
+++ src/main/kotlin/com/kaizen/airKaizen/service/RouteService.kt
@@ -4,52 +4,56 @@ import com.kaizen.airKaizen.model.Node
 import org.springframework.stereotype.Service
 
 @Service
-class AirKaizenService {
-
-    val path = arrayListOf<Node>()
-    val paths = arrayListOf<List<Node>>()
+class RouteService {
 
     @ExperimentalStdlibApi
     fun findPaths(origin: Node, destination: Node): List<List<Node>> {
 
         var layovers = 0
+        val path = arrayListOf<Node>()
+        val paths = arrayListOf<List<Node>>()
 
         path.add(origin)
         origin.edges.forEach {
-            evaluate(it, destination, layovers)
+            evaluate(it, destination, layovers, path, paths)
         }
-
-        println("number of paths: ${paths.size}")
 
         return paths
     }
 
     @ExperimentalStdlibApi
-    private fun dfs(node: Node, destination: Node, layovers: Int) {
+    private fun dfs(
+        node: Node,
+        destination: Node,
+        layovers: Int,
+        path: ArrayList<Node>,
+        paths: ArrayList<List<Node>>
+    ) {
         node.visited = true
         node.edges.forEach {
-            evaluate(it, destination, layovers)
+            evaluate(it, destination, layovers, path, paths)
         }
     }
 
     @ExperimentalStdlibApi
-    private fun evaluate(current: Node, destination: Node, layovers: Int){
-        if (condition(current, destination, layovers)) {
+    private fun evaluate(
+        current: Node,
+        destination: Node,
+        layovers: Int,
+        path: ArrayList<Node>,
+        paths: ArrayList<List<Node>>
+    ){
+        if (condition(current, destination, layovers, path)) {
             path.add(current)
-            dfs(current, destination, layovers + 1)
+            dfs(current, destination, layovers + 1, path, paths)
             current.visited = false
             path.removeLast()
         } else {
             if (destination.name == current.name) {
                 // save the path
-                paths.add(path.toList())
-                // print path
-                print("path: ")
-                for (node in path) {
-                    print("${node.name}  ," )
-                }
-                print("${destination.name}" )
-                println()
+                val currentPath = path.toMutableList()
+                currentPath.add(destination)
+                paths.add(currentPath)
             }
         }
     }
@@ -61,7 +65,12 @@ class AirKaizenService {
      * - find the destination
      * - node not included as part of the list of results
      */
-    private fun condition(node: Node, destination: Node, layovers: Int): Boolean {
+    private fun condition(
+        node: Node,
+        destination: Node,
+        layovers: Int,
+        path: ArrayList<Node>
+    ): Boolean {
         return !node.visited && layovers <= 1 && destination.name != node.name && !path.contains(node)
     }
 
---
+++ src/test/kotlin/com/kaizen/airKaizen/service/RouteServiceTest.kt
@@ -0,0 +1,63 @@
+package com.kaizen.airKaizen.service
+
+import com.kaizen.airKaizen.model.Node
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RouteServiceTest(
+    @Qualifier("graph") val graph: ArrayList<Node>,
+    @Autowired val routeService: RouteService
+) {
+
+    @ExperimentalStdlibApi
+    @Test
+    fun `should retrieve all the paths`(){
+        // Arrange, I'm assuming that they will exist after the search
+        val destination = graph.find { it.name == "5" }
+        val origin = graph.find { it.name == "1" }
+
+        // Act
+        val paths = routeService.findPaths(origin!!, destination!!)
+
+        // print path
+        println("number of paths: ${paths.size}")
+        paths.forEach {
+            print("path: ")
+            it.forEach { each -> print("${each.name}," ) }
+            println()
+        }
+
+        // Assert
+        Assertions.assertEquals(10, paths.size)
+    }
+
+    @ExperimentalStdlibApi
+    @Test
+    fun `should retrieve paths with size less than 4`(){
+        // Arrange, I'm assuming that they will exist after the search
+        val destination = graph.find { it.name == "5" }
+        val origin = graph.find { it.name == "1" }
+
+        // Act
+        val paths = routeService.findPaths(origin!!, destination!!)
+
+        // print path
+        println("number of paths: ${paths.size}")
+        paths.forEach {
+            print("path: ")
+            it.forEach { each -> print("${each.name}," ) }
+            println()
+        }
+
+        // Assert
+        paths.forEach {
+            Assertions.assertTrue(it.size <= 4)
+        }
+
+    }
+
+}
